39f30e6d1ef5d90ce3352fcea84fc053b37fb013
OfficialKotik <mariakamenskyh@gmail.com>
2020-11-01 16:48:49 +0200

    App

---
+++ Photos/Photos/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Photos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new VM();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //! added using Plugin.Media;
+            await CrossMedia.Current.Initialize();
+            var image = new Image();
+            //// if you want to take a picture use this
+            // if(!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            /// if you want to select from the gallery use this
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
+                return;
+            }
+
+            //! added using Plugin.Media.Abstractions;
+            // if you want to take a picture use StoreCameraMediaOptions instead of PickMediaOptions
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+          /*  if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "Could not get the image, please try again.", "Ok");
+                return;
+            }
+          */
+            image.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+            var page = new VM();
+            page.Types.Add(image);
+        }
+    }
+}
---
+++ Photos/Photos/VM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace Photos
+{
+    class VM : INotifyPropertyChanged
+    {
+        //  public Command Photo { get; set; }
+        public ObservableCollection<Image> types { get; set; }
+        public ObservableCollection<Image> Types { get => types; set { types = value; OnPropertyChanged("Types"); } }
+        public VM()
+        {
+            //  Photo = new Command(OnPickPhotoButtonClicked);
+            Types = new ObservableCollection<Image>();
+            Types.Add(new Image() { Source = "heart", BackgroundColor = Color.White });
+            Types.Add(new Image() { Source = "heart", BackgroundColor = Color.White });
+            Types.Add(new Image() { Source = "heart", BackgroundColor = Color.White });
+            Types.Add(new Image() { Source = "heart", BackgroundColor = Color.White });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
